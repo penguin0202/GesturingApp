8c120315183784613ca834584bb2c674d2a1be32
penguin0202 <holypenguin.david@gmail.com>
2026-07-10 21:23:56 +0800

    first steps

---
+++ main.py
@@ -0,0 +1,10 @@
+import mediapipe
+import cv2
+import pygame
+
+# run pygame window
+# input video
+# continuously get hand gestures (make functions for each)
+# draw mountains and trees based on gestures
+# z-axis
+# what the fuck is this ordering
